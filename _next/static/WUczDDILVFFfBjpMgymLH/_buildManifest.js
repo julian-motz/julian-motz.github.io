bb17bee466e979d9b86e75f4242a7fd70f6e32ba
julian-motz <107186111+julian-motz@users.noreply.github.com>
2022-06-16 12:50:39 +0000

    Deploying to gh-pages from @ julian-motz/julian-motz.github.io@5f34e79befb9116b20a0c698e91aaa25e2e1e666 🚀

--- _next/static/WUczDDILVFFfBjpMgymLH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/533-c3bb466fe366de0d.js","static/chunks/pages/index-c0e47523f0cae17a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
